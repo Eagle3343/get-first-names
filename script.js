bc4e18e2d4c2a9bae61edde8f7276df2db370936
Eagle3343 <Eagle3343>
2024-12-11 17:58:30 +0400

    code added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // Good Luck 💪🏾
+let names =[["Ahmed","Aisha","Amina"],
+           ["Jaamac","Khadijah","Layla"],
+           ["Mohamed","Ali"] ];
+
+let newName=names[i];
+
+function getfirstName(){
+
+    for (let i = 0; i < names.length; i++) {
+       //for (let j = 0; j < names[i].length; j++) {
+       console.log(newName[i][0]);
+    
+               
+    }
+
+
+}
+
+getfirstName();
